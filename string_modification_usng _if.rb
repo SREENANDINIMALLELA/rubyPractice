7768e09d28feac5c1fd6c86eb12c06dc938e6d55
sreenandini <mallelasreenandini2@gmail.com>
2019-08-18 14:40:18 +0100

    update

--- string_modification_usng _if.rb
+++ string_modification_usng _if.rb
@@ -1,1 +1,13 @@
+print "Thtring, pleathe!: "
+user_input = gets.chomp
+user_input.downcase!
+
+#first it checks for the string that is included in the user input if there it will use global sustitution method to substitute that string
+
+if user_input.include? "s"
+  user_input.gsub!(/s/, "th")
+else
+  puts "Nothing to do here!"
+end
+
 puts "Your string is: #{user_input}"
