c9d2865d69b516ffbf6af6ca323fbb0668a9c4dc
sreenandini <mallelasreenandini2@gmail.com>
2019-08-18 14:53:19 +0100

    update

--- whlie_until_Loop.rb
+++ whlie_until_Loop.rb
@@ -1,8 +1,12 @@
 #if we want to perform a repeat action if the condition is true and dont know how many times we use while
-counter = 1
-while counter < 11
-  puts counter
-  counter = counter + 1
+#It is like filling a bucket with water using a mug. You never know how many mugs of water you use, but only be stopped when the bucket is full.
+#Work until satisfying a given condition.
+mug = 1
+print " how many mugs of neede to fill the bucket of water ?"
+number_of_mugs = Integer(gets.chomp)
+while  mug < number_of_mugs
+  puts mug
+  mug = mug + 1
 end
 # untill is the backword while
 counter = 1
