019f850fe5f95bd98629b96b2994b342d6c253ee
sreenandini <mallelasreenandini2@gmail.com>
2019-08-18 14:37:09 +0100

    while

---
+++ whlieLoop.rb
@@ -0,0 +1,6 @@
+#if we want to perform a repeat action if the condition is true and dont know how many times we use while
+counter = 1
+while counter < 11
+  puts counter
+  counter = counter + 1
+end
